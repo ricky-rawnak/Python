b7ccbcbcf051ff68094dcf71ef9d5d59c005e57c
Ricky Rawnak Chowdhury <rickyrawnak@gmail.com>
2024-01-17 12:04:46 +0600

    new

---
+++ 45-100/user.py
@@ -0,0 +1,8 @@
+
+#------------------------------ if __name__ == "__main__"
+
+def greetings():
+    print("Hey you are welcome from main name")
+print(__name__)
+if __name__=="__main__":
+    greetings()
